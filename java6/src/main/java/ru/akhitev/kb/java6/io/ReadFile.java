2ad41942787e0fba23f910f85e4e586dcefdba2a
Aleksei Khitev <alexkhitev@gmail.com>
2019-09-03 09:09:32 +0300

    IO

---
+++ java6/src/main/java/ru/akhitev/kb/java6/io/ReadFile.java
@@ -0,0 +1,32 @@
+package ru.akhitev.kb.java6.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class ReadFile {
+    public static void main(String[] args) throws URISyntaxException {
+        String filePath = prepareFileNameFromResources();
+        String result = null;
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+            }
+            result = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result);
+    }
+
+    private static String prepareFileNameFromResources() throws URISyntaxException {
+        URL resource = ReadFile.class.getResource("/file_reader_test_file.txt");
+        return resource.toURI().getPath();
+    }
+}
---
+++ java6/src/main/java/ru/akhitev/kb/java6/io/StringReaderExample.java
@@ -0,0 +1,15 @@
+package ru.akhitev.kb.java6.io;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class StringReaderExample {
+    public static void main(String[] args) throws IOException {
+        String inputString = "12345\n67890";
+        StringReader stringReader = new StringReader(inputString);
+        int currentSybmol;
+        while ((currentSybmol = stringReader.read()) != -1) {
+            System.out.print((char) currentSybmol);
+        }
+    }
+}
