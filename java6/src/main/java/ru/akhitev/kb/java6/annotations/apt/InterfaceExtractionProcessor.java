3ed8c1067a4963b69cc3e57609ab36e47d4b0e50
Aleksei Khitev <alexkhitev@gmail.com>
2019-09-17 08:37:11 +0300

    окончание аннотаций

---
+++ java6/src/main/java/ru/akhitev/kb/java6/annotations/apt/InterfaceExtractionProcessor.java
@@ -0,0 +1,18 @@
+package ru.akhitev.kb.java6.annotations.apt;
+
+import javax.annotation.processing.ProcessingEnvironment;
+
+/**
+ * В Java 8 многих классов нет.
+ * есть https://docs.oracle.com/javase/6/docs/api/javax/annotation/processing/package-summary.html
+ * и внешняя библиотека где то (но не факт, что нормальная)
+ */
+public class InterfaceExtractionProcessor {
+//    private final ProcessingEnvironment processingEnvironment;
+//    private List<Method>
+//
+//    public InterfaceExtractionProcessor(ProcessingEnvironment processingEnvironment) {
+//        this.processingEnvironment = processingEnvironment;
+//        InterfaceExtractionProcessor.class.getMe
+//    }
+}
