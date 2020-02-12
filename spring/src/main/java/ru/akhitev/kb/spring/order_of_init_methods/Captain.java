b8058a5b1d8f3cb4a1fac73e247c46070a15f410
Aleksei Khitev <alexkhitev@gmail.com>
2020-02-12 16:21:17 +0300

    Порядок вызовов методов уничтожния

--- spring/src/main/java/ru/akhitev/kb/spring/order_of_init_methods/Captain.java
+++ spring/src/main/java/ru/akhitev/kb/spring/order_of_init_methods/Captain.java
@@ -1,10 +1,12 @@
 package ru.akhitev.kb.spring.order_of_init_methods;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class Captain implements InitializingBean {
+public class Captain implements InitializingBean, DisposableBean {
     public void init() {
         System.out.println("3. Use buff skills");
     }
@@ -17,5 +19,20 @@ public class Captain implements InitializingBean {
     @PostConstruct
     public void initializing() {
         System.out.println("1. Check skills, set and weapon");
+    }
+
+    public void deconstruct() {
+        System.out.println("3. Travel to a Bank");
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("2. Roll valuable items from boss chest");
+    }
+
+    @PreDestroy
+    private void deconstructing() {
+        System.out.println("1. Wear lucky horseshoe");
     }
 }
