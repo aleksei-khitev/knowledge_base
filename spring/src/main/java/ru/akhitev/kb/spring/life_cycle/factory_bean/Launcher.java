938fb6fa9ab7b94920fc8b94add829ce4898bc62
Aleksei Khitev <alexkhitev@gmail.com>
2020-02-13 12:10:29 +0300

    factory_bean и factory_method

--- spring/src/main/java/ru/akhitev/kb/spring/life_cycle/factory_bean/Launcher.java
+++ spring/src/main/java/ru/akhitev/kb/spring/life_cycle/factory_bean/Launcher.java
@@ -7,5 +7,15 @@ public class Launcher {
     public static void main(String[] args) {
         ApplicationContext context = new GenericXmlApplicationContext("spring/life_cycle/factory_bean/beans.xml");
         System.out.println(context.getBean(Reinforcement.class).getIsd());
+        getFactoryBeanGetObjectFromContext(context);
+        getFactoryBeanItSelf(context);
+    }
+
+    public static void getFactoryBeanGetObjectFromContext(ApplicationContext context) {
+        System.out.println(context.getBean("kuatShipYard"));
+    }
+
+    public static void getFactoryBeanItSelf(ApplicationContext context) {
+        System.out.println(context.getBean("&kuatShipYard"));
     }
 }
