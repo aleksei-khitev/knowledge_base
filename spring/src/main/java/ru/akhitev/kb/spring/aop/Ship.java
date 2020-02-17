015daf5b76f0c4b962c982107fe607f8ffee5f43
Aleksei Khitev <alexkhitev@gmail.com>
2020-02-17 12:20:24 +0300

    aspect

---
+++ spring/src/main/java/ru/akhitev/kb/spring/aop/Ship.java
@@ -0,0 +1,8 @@
+package ru.akhitev.kb.spring.aop;
+
+public class Ship {
+    public void jump(String targetStarSystem) {
+        System.out.println("preparing for jump");
+        System.out.println("jump");
+    }
+}
