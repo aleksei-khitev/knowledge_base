a60423b3ee4af5ed4546245b11f14ef851e4e28a
Aleksei Khitev <alexkhitev@gmail.com>
2020-02-23 16:09:48 +0300

    new db structure

--- spring/src/main/resources/spring/envers/schema.sql
+++ spring/src/main/resources/spring/envers/schema.sql
@@ -2,20 +2,12 @@ CREATE TABLE space_state (
  id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(100) NOT NULL UNIQUE,
  version INT NOT NULL DEFAULT 0,
- created_by VARCHAR(20),
- created_date TIMESTAMP,
- last_modified_by VARCHAR(20),
- last_modified_date TIMESTAMP,
  PRIMARY KEY(id)
 );
 CREATE TABLE command_rank (
  id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(50) NOT NULL UNIQUE,
  version INT NOT NULL DEFAULT 0,
- created_by VARCHAR(20),
- created_date TIMESTAMP,
- last_modified_by VARCHAR(20),
- last_modified_date TIMESTAMP,
  PRIMARY KEY(id)
 );
 CREATE TABLE fleet_unit (
@@ -23,10 +15,6 @@ CREATE TABLE fleet_unit (
  name VARCHAR(50) NOT NULL UNIQUE,
  minimum_command_rank_id VARCHAR(50),
  version INT NOT NULL DEFAULT 0,
- created_by VARCHAR(20),
- created_date TIMESTAMP,
- last_modified_by VARCHAR(20),
- last_modified_date TIMESTAMP,
  PRIMARY KEY(id),
  CONSTRAINT fk_command_rank FOREIGN KEY(minimum_command_rank_id) REFERENCES command_rank(id)
 );
@@ -36,10 +24,6 @@ CREATE TABLE ship (
  ship_class VARCHAR(50) NOT NULL,
  link VARCHAR(200) NOT NULL,
  version INT NOT NULL DEFAULT 0,
- created_by VARCHAR(20),
- created_date TIMESTAMP,
- last_modified_by VARCHAR(20),
- last_modified_date TIMESTAMP,
  UNIQUE uq_ship_1 (type, ship_class),
  PRIMARY KEY(id),
 );
