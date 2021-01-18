58ad3b1940a6d05867019382bd8d961898aa226e
Aleksei Khitev <aleksei.khitev@clarivate.com>
2021-01-18 11:34:46 +0300

    nested progs

---
+++ pl sql/fundamentals/nested.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE PROCEDURE calculate_bonus (good_calls IN NUMBER, averall_calls IN NUMBER)
+IS
+    bonus_low_border NUMBER := 95;
+    good_calls_percentage NUMBER;
+
+    PROCEDURE compute_percentage
+    IS
+    BEGIN
+        good_calls_percentage := good_calls * 100 / averall_calls;
+    END compute_percentage;
+BEGIN
+    compute_percentage;
+    DBMS_OUTPUT.PUT_LINE('Percentage of good calls = ' || good_calls_percentage);
+    IF good_calls_percentage >= bonus_low_border
+    THEN
+        DBMS_OUTPUT.PUT_LINE('Bonus accepted');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('No bonus');
+    END IF;
+END calculate_bonus;
+/
+
+EXECUTE calculate_bonus(94, 100);
+EXECUTE calculate_bonus(96, 100);
--- pl sql/fundamentals/pkg_with_variable.sql
+++ pl sql/fundamentals/pkg_with_variable.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE PACKAGE eso
 IS
-    resistance_cap NUMBER;
+    resistance_cap NUMBER := 33;
 END eso;
 /
 
 BEGIN
-    eso.resistance_cap := 33;
     DBMS_OUTPUT.PUT_LINE('resistance cap = ' || eso.resistance_cap);
 END;
 /
