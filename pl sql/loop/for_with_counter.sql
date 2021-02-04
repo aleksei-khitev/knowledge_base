783ee2bb2445fc88a1ddb7bdaa0b1a9c87e42d63
Aleksei Khitev <aleksei.khitev@clarivate.com>
2021-02-04 11:28:06 +0300

    pl sql for with counter

---
+++ pl sql/loop/for_with_counter.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE simple_counter(min_border IN INTEGER, max_border IN INTEGER)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Borders = [' || min_border || ', ' || max_border || ']');
+    DBMS_OUTPUT.PUT_LINE('Iteration with normal order');
+    DBMS_OUTPUT.PUT_LINE('---------------------------');
+    FOR iterator_variable IN min_border .. max_border
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Iterator Value = ' || iterator_variable);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('---------------------------');
+    DBMS_OUTPUT.PUT_LINE('Iteration with reverse order');
+    DBMS_OUTPUT.PUT_LINE('---------------------------');
+    FOR iterator_variable IN REVERSE min_border .. max_border
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Iterator Value = ' || iterator_variable);
+    END LOOP;
+END;
+/
+
+EXEC simple_counter(1, 10)
