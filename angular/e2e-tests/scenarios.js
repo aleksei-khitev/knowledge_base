73afad29da3699caa7538ca401003fca9fb076c2
Aleksei Khitev <aleksei.khitev1@thomsonreuters.com>
2021-02-07 13:48:36 +0300

    angulajs e-2-e test

--- angular/e2e-tests/scenarios.js
+++ angular/e2e-tests/scenarios.js
@@ -3,10 +3,28 @@
 // AngularJS E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('My app', function() {
+describe('StatusReader Application', function() {
 
-  beforeEach(function() {
-    browser.get('index.html');
+  describe('statusList', function() {
+
+    beforeEach(function() {
+      browser.get('');
+    });
+
+    it('should filter the status list as a user types into the search box', function() {
+      var phoneList = element.all(by.repeater('status in $ctrl.statuses'));
+      var query = element(by.model('$ctrl.query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('Tomcat');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('Apache');
+      expect(phoneList.count()).toBe(1);
+    });
+
   });
 
 });
