be25f73f7022e4960f6fba2b5fa44bffeed2efc1
Aleksei_Khitev <aleksei_khitev@rntgroup.com>
2024-01-09 07:04:28 +0300

    updated

---
+++ 1. Tech Skills/1. Языки программирования/1. Java/102. Каты/katas/ide/IdeKata.java
@@ -0,0 +1,28 @@
+package ide;
+
+/**
+ * Выполнить 5 кат
+ */
+public class IdeKata {
+    /**
+     * 1. Запустить через `Shift` + `F10`
+     * 2. Поставить Break Point через `Ctrl` + `F8`
+     * 3. Запустить через `Shift` + `F9`
+     */
+    public static void main(String[] args) {
+        int a = 0;
+        long b = 1L;
+
+        // 4.
+        // вызвать `substitutionsKata`
+        // для подстановки подходящего параметра использовать `Ctrl` + `Shift` + `Space`
+
+
+    }
+
+    private static void substitutionsKata(long value) {
+        // 5.
+        // Завернуть в try-catch используя `Ctrl` + `Alt` + `T`
+        System.out.println(value);
+    }
+}
